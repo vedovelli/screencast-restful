d3ad5728afb01a34854942a1df3ec9672f440c84
Nicolas França <nicolasfranca9@gmail.com>
2014-07-18 15:16:05 -0300

    Melhorando apresentação das variáveis

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = require('./app_config.js'),
     userController = require('./controller/userController.js'),
-    validator = require('validator'),
+    validator = require('validator');
 
-function validate(attribute) {
+function valid(attribute) {
   return validator.trim(validator.escape(req.param(attribute)));
 }
 
@@ -19,26 +19,30 @@ app.get('/users', function(req, res) {
 });
 
 app.get('/users/:id', function(req, res) {
-  var id = validate('id');
+  var id = valid('id');
+
   userController.user(id, responseJSON(resp));
 });
 
 app.post('/users', function(req, res) {
-  var fullname = validate('fullname');
-  var email = validate('email');
-  var password = validate('password');
+  var fullname = valid('fullname'),
+      email = valid('email'),
+      password = valid('password');
+
   userController.save(fullname, email, password, responseJSON(resp));
 });
 
 app.put('/users', function(req, res) {
-  var id = validate('id');
-  var fullname = validate('fullname');
-  var email = validate('email');
-  var password = validate('password');
+  var id = valid('id'),
+      fullname = valid('fullname'),
+      email = valid('email'),
+      password = valid('password');
+
   userController.update(id, fullname, email, password, responseJSON(resp));
 });
 
 app.delete('/users/:id', function(req, res) {
-  var id = validate('id');
+  var id = valid('id');
+
   userController.delete(id, responseJSON(resp));
 });
